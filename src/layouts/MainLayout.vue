ea93503b32eb7c91b69636a4b4ae21bbabeb8a87
Dmitry Visimskikh <drimetolle@gmail.com>
2020-02-24 18:49:02 +0500

    add layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="#d94d33"
+      dark
+    >
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer
+      color="#d94d33"
+      app
+    >
+      <span class="white--text">&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+    props: {
+        source: String
+    },
+    data: () => ({
+        drawer: null
+    })
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
-import Login from '@/views/login/loginForm.vue'
+import Login from '@/views/auth/LoginForm.vue'
+import Registration from '@/views/auth/RegistrationForm.vue'
 import store from '@/store'
 import Configurator from '@/views/Configurator.vue'
 
@@ -28,12 +28,6 @@ const ifAuthenticated = (to, from, next) => {
 
 const routes = [
     {
-        path: '/home',
-        name: 'home',
-        component: Home,
-        beforeEnter: ifNotAuthenticated
-    },
-    {
         path: '/about',
         name: 'about',
         component: About,
@@ -42,7 +36,14 @@ const routes = [
     {
         path: '/login',
         name: 'login',
+        meta: { layout: 'empty' },
         component: Login
+    },
+    {
+        path: '/registration',
+        name: 'registration',
+        meta: { layout: 'empty' },
+        component: Registration
     },
     {
         path: '/',
