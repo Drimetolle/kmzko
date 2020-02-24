edc5dd833b0dc9a8f461033338ec70292a5369ea
Dmitry Visimskikh <drimetolle@gmail.com>
2020-02-24 18:50:34 +0500

    add currency filter

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import currencyFilter from './filters/currency.filter'
 
 Vue.config.productionTip = false
+Vue.filter('currency', currencyFilter)
 
 new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App)
 }).$mount('#app')
