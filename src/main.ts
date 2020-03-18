c8b2432aec818c183c20a02dfd4f2ef367fe846a
Dmitry Visimskikh <drimetolle@gmail.com>
2020-03-18 14:30:54 +0500

    imports

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import currencyFilter from './filters/currency.filter.js'
+import currencyFilter from './filters/currency.filter.ts'
+import rateFilter from './filters/rate.filter.ts'
 import axios from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.filter('currency', currencyFilter)
+Vue.filter('characteristic', rateFilter)
 
 new Vue({
   router,
