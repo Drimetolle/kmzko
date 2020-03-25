a26991f086511fb6bb6ddbfd7f2f0a0885ef3f02
Dmitry Visimskikh <drimetolle@gmail.com>
2020-03-25 12:43:58 +0500

    transfer locale to json file

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -3,24 +3,21 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-const messages = {
-  en: {
-    'question-list': 'Questionnaire',
-    'listof-conveyors': 'List of conveyors',
-    'tape': 'tape',
-    'submit': 'submit',
-    'edit': 'edit',
-  },
-  ru: {
-    'question-list': 'Опросный лист',
-    'listof-conveyors': 'Список конвейеров',
-    'tape': 'Ленточный',
-    'submit': 'Отправить',
-    'edit': 'Редактировать',
-  },
+function loadLocaleMessages () {
+  const locales = require.context('./../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const messages = Object()
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1]
+      messages[locale] = locales(key)
+    }
+  })
+  return messages
 }
 
 export default new VueI18n({
-  locale: 'ru',
-  messages,
+  locale: process.env.VUE_APP_I18N_LOCALE || 'ru',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  messages: loadLocaleMessages(),
 })
