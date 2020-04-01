4220d0dd7c610b6783b4649aca2077a01503b5da
Dmitry Visimskikh <drimetolle@gmail.com>
2020-04-01 15:23:51 +0500

    filter units

--- src/components/ConveyorCard.vue
+++ src/components/ConveyorCard.vue
@@ -1,17 +1,17 @@
 <template>
   <v-expansion-panel>
-    <v-expansion-panel-header>{{this.conveyor.name}}</v-expansion-panel-header>
+    <v-expansion-panel-header>{{ this.conveyor.name }}</v-expansion-panel-header>
     <v-expansion-panel-content>
       <div v-for="(node, i) in this.conveyor.nodes"
           :key="i"
       >
         <v-list-item>
           <v-list-item-content>
-            <v-list-item-title class="title">{{node.name}}</v-list-item-title>
+            <v-list-item-title class="title">{{ node.name }}</v-list-item-title>
             <v-list-item v-for="(detail, i) in node.details"
                 :key="i"
             >
-              <v-list-item-title>{{`${detail.name}: `}}{{characteristics(detail)}}</v-list-item-title>
+              <v-list-item-title>{{ `${detail.name}: ` }} {{ characteristics(detail) | characteristic }}</v-list-item-title>
             </v-list-item>
           </v-list-item-content>
         </v-list-item>
@@ -42,6 +42,7 @@
 import Vue from 'vue'
 import { Conveyor, Detail, States } from '@/types/index'
 import { mapGetters, mapMutations } from 'vuex'
+import { CharacteristicDto } from '@/types/index'
 
 export default Vue.extend({
   props: {
@@ -63,9 +64,9 @@ export default Vue.extend({
       // console.log(this.conveyor)
       return this.conveyor.nodes[index].details.map(n => n.name).join(', ')
     },
-    characteristics(detail: Detail): string {
+    characteristics(detail: Detail): Array<CharacteristicDto> {
       // console.log(detail)
-      return detail.characteristics.map(n => `${n.name} - ${n.value}${n.type.substring(0, 1)}`).join(', ')
+      return detail.characteristics
     },
     submit() {
       this.$emit('selectConveyor', this.conveyor.id)
--- src/filters/rate.filter.ts
+++ src/filters/rate.filter.ts
@@ -1,3 +1,13 @@
-export default function rateFilter(value: string, rate = 'kg') {
-    return `${value} ${rate}`
+import { CharacteristicDto } from '@/types/index'
+
+export default function rateFilter(value: Array<CharacteristicDto>) {
+  const formatArray = value.map(detail => `${detail.name} - ${detail.value}${map.get(detail.type) || ''}`)
+  return formatArray.join(', ')
 }
+
+const map = new Map<string, string>([
+  ['meter', 'm'],
+  ['kilogram', 'kg'],
+  ['watt', 'W'],
+  ['cross', 'Ø'],
+])
